b7d43bdce51178ff8ccb6ee13ca0ed39e078b96c
Vinothkumar Sevagamurthy <vinothksc@gmail.com>
2020-04-11 19:49:37 +0530

    Add files via upload

---
+++ OddNumbers.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class OddNumbers {
+
+	public static void main(String[] args) {
+		int number = 20; 
+		
+		for (int i = 1; i <= number; i++) {
+			if (i%2 != 0) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
